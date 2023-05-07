06109e347c2e2083081adb3a4a5e32f43bdda6a2
Sergei Popov <sergei.v.popov@icloud.com>
2023-05-07 13:53:09 +0300

    feat: response to 'post' request

--- bazito/ads/views.py
+++ bazito/ads/views.py
@@ -1,9 +1,13 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from ads.models import AdsModel
 
 
+@csrf_exempt
 def page_ads(request):
     if request.method == "GET":
         ads = AdsModel.objects.all()
@@ -24,7 +28,15 @@ def page_ads(request):
                             status=200)
 
     elif request.method == 'POST':
-        return JsonResponse({'Status': 'ADS'},
+        ad_data = json.loads(request.body)
+        ad = AdsModel.objects.create(
+            name=ad_data['name'],
+            author=ad_data['author'],
+            price=ad_data['price'],
+            description=ad_data['description'],
+            is_published=ad_data['is_published'],
+            )
+        return JsonResponse({'Status': 'OK'},
                             safe=False,
                             json_dumps_params={'ensure_ascii': False},
                             status=201)
--- bazito/categories/views.py
+++ bazito/categories/views.py
@@ -1,18 +1,36 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from categories.models import CatModel
 
 
+@csrf_exempt
 def page_categories(request):
-    categories = CatModel.objects.all()
-    response = []
-    for item in categories:
-        response.append({
-            'id': item.id,
-            'name': item.name
-        })
-    return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False}, status=200)
+    if request.method == "GET":
+        categories = CatModel.objects.all()
+        response = []
+        for item in categories:
+            response.append({
+                'id': item.id,
+                'name': item.name
+            })
+        return JsonResponse(response,
+                            safe=False,
+                            json_dumps_params={'ensure_ascii': False},
+                            status=200)
+
+    elif request.method == 'POST':
+        cat_data = json.loads(request.body)
+        cat = CatModel.objects.create(
+            name=cat_data['name'],
+        )
+        return JsonResponse({'Status': 'OK'},
+                            safe=False,
+                            json_dumps_params={'ensure_ascii': False},
+                            status=201)
 
 
 def page_cat_by_id(request, pk: int):
@@ -23,7 +41,7 @@ def page_cat_by_id(request, pk: int):
                 'id': ad.id,
                 'name': ad.name,
                 }
-        except AdsModel.DoesNotExist:
+        except CatModel.DoesNotExist:
             return JsonResponse({"error": "Not found"},
                                 status=404)
 
