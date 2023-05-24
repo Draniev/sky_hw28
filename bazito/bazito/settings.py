3d8278bf79b8a42ecff6af2b3bf21107d71f9e47
Sergei Popov <sergei.v.popov@icloud.com>
2023-05-24 15:25:47 +0300

    postgres bd added

--- bazito/bazito/settings.py
+++ bazito/bazito/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ads',
-    'categories',
 ]
 
 MIDDLEWARE = [
@@ -76,9 +75,17 @@ WSGI_APPLICATION = 'bazito.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'qazxswedc123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
