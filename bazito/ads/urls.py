eaa15e548116c49992ff8158e47b628b78eef3f4
Sergei Popov <sergei.v.popov@icloud.com>
2023-05-24 19:33:18 +0300

    CRUD for ads and cats

--- bazito/ads/urls.py
+++ bazito/ads/urls.py
@@ -1,10 +1,17 @@
+from ads.views import (AdCreateView, AdDeleteView, AdDetailView, AdsView,
+                       AdUpdateView, CatCreateView, CatDeleteView,
+                       CatDetailView, CatUpdateView, CatsView)
 from django.urls import path
-
-from ads.views import AdsView, AdDetailView, CatView, CatDetailView
 
 urlpatterns = [
     path('', AdsView.as_view()),
+    path('/create/', AdCreateView.as_view()),
     path('<int:pk>/', AdDetailView.as_view()),
-    path('cat/', CatView.as_view()),
-    path('cat/<int:pk>/', CatDetailView.as_view())
+    path('<int:pk>/update/', AdUpdateView.as_view()),
+    path('<int:pk>/delete/', AdDeleteView.as_view()),
+    path('cat/', CatsView.as_view()),
+    path('cat/create/', CatCreateView.as_view()),
+    path('cat/<int:pk>/', CatDetailView.as_view()),
+    path('cat/<int:pk>/update/', CatUpdateView.as_view()),
+    path('cat/<int:pk>/delete/', CatDeleteView.as_view()),
 ]
--- bazito/ads/views.py
+++ bazito/ads/views.py
@@ -1,43 +1,91 @@
 import json
 
+from ads.models import AdsModel, CatModel
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import DetailView
-
-from ads.models import AdsModel, CatModel
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class AdsView(View):
-    def get(self, request):
-        ads = AdsModel.objects.all()
+class AdsView(ListView):
+    model = AdsModel
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        ads = self.object_list
         response = []
 
         for ad in ads:
             response.append({
                 'id': ad.id,
                 'name': ad.name,
-                'author': ad.author,
+                'author': ad.author.username,
                 'price': ad.price,
                 'description': ad.description,
                 'is_published': ad.is_published,
+                'category': ad.category.name,
+                # 'image': ad.image,
             })
         return JsonResponse(response,
                             safe=False,
                             json_dumps_params={'ensure_ascii': False},
                             status=200)
 
-    def post(self, request):
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AdCreateView(CreateView):
+    model = AdsModel
+    fields = ['name', 'author_id', 'price',
+              'description', 'category_id', 'is_published']
+
+    def post(self, request, *args, **kwargs):
         ad_data = json.loads(request.body)
         AdsModel.objects.create(
             name=ad_data['name'],
-            author=ad_data['author'],
+            author_id=ad_data['author_id'],
             price=ad_data['price'],
             description=ad_data['description'],
             is_published=ad_data['is_published'],
-            )
+            category_id=ad_data['category_id'],
+        )
+        return JsonResponse({'Status': 'OK'},
+                            safe=False,
+                            json_dumps_params={'ensure_ascii': False},
+                            status=201)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AdUpdateView(UpdateView):
+    model = AdsView
+    fields = ['name', 'price', 'description', 'category_id', 'is_published']
+
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        ad_data = json.loads(request.body)
+
+        self.object.name = ad_data['name']
+        self.object.price = ad_data['price']
+        self.object.description = ad_data['description']
+        self.object.category_id = ad_data['category_id']
+        self.object.is_published = ad_data['is_published']
+        self.object.save()
+        return JsonResponse({'Status': 'OK'},
+                            safe=False,
+                            json_dumps_params={'ensure_ascii': False},
+                            status=201)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AdDeleteView(DeleteView):
+    model = AdsModel
+    success_url = '/'
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
         return JsonResponse({'Status': 'OK'},
                             safe=False,
                             json_dumps_params={'ensure_ascii': False},
@@ -49,16 +97,18 @@ class AdDetailView(DetailView):
     model = AdsModel
 
     def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
         try:
             ad = self.get_object()
             response = {
                 'id': ad.id,
                 'name': ad.name,
-                'author': ad.author,
+                'author': ad.author.username,
                 'price': ad.price,
                 'description': ad.description,
                 'is_published': ad.is_published,
-                }
+                'category': ad.category.name,
+            }
         except AdsModel.DoesNotExist:
             return JsonResponse({"error": "Not found"},
                                 status=404)
@@ -70,9 +120,13 @@ class AdDetailView(DetailView):
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class CatView(View):
-    def get(self, request):
-        categories = CatModel.objects.all()
+class CatsView(ListView):
+    model = CatModel
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        categories = self.object_list
         response = []
         for item in categories:
             response.append({
@@ -84,7 +138,13 @@ class CatView(View):
                             json_dumps_params={'ensure_ascii': False},
                             status=200)
 
-    def post(self, request):
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CatCreateView(CreateView):
+    model = CatModel
+    fields = ['name']
+
+    def post(self, request, *args, **kwargs):
         cat_data = json.loads(request.body)
         CatModel.objects.create(name=cat_data['name'])
         return JsonResponse({'Status': 'OK'},
@@ -94,16 +154,47 @@ class CatView(View):
 
 
 @method_decorator(csrf_exempt, name='dispatch')
+class CatUpdateView(UpdateView):
+    model = CatModel
+    fields = ['name']
+
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        cat_data = json.loads(request.body)
+
+        self.object.name = cat_data['name']
+        self.object.save()
+        return JsonResponse({'Status': 'OK'},
+                            safe=False,
+                            json_dumps_params={'ensure_ascii': False},
+                            status=201)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CatDeleteView(DeleteView):
+    model = CatModel
+    success_url = '/'
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
+        return JsonResponse({'Status': 'OK'},
+                            safe=False,
+                            json_dumps_params={'ensure_ascii': False},
+                            status=201)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
 class CatDetailView(DetailView):
     model = CatModel
-    
+
     def get(self, request, *args, **kwargs):
         try:
-            ad = self.get_object()
+            cat = self.get_object()
             response = {
-                'id': ad.id,
-                'name': ad.name,
-                }
+                'id': cat.id,
+                'name': cat.name,
+            }
         except CatModel.DoesNotExist:
             return JsonResponse({"error": "Not found"},
                                 status=404)
