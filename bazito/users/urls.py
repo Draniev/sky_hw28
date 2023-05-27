7037923129af7075a3a0f49acaa1fa6798152d0c
Sergei Popov <sergei.v.popov@icloud.com>
2023-05-27 13:53:41 +0300

    fix: remuve second \ from url

--- bazito/users/urls.py
+++ bazito/users/urls.py
@@ -5,7 +5,7 @@ from users.views import (UserCreateView, UserDeleteView, UserUpdateView,
 urlpatterns = [
     path('', UsersView.as_view()),
     # path('<int:pk>/', UserDetailView.as_view()),
-    path('/create/', UserCreateView.as_view()),
+    path('create/', UserCreateView.as_view()),
     path('<int:pk>/update', UserUpdateView.as_view()),
     path('<int:pk>/delete', UserDeleteView.as_view()),
     # path('cat/<int:pk>/', CatDetailView.as_view())
