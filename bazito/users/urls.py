3c89ed61ee2dc006ed06bcb33f706dba63282174
Sergei Popov <sergei.v.popov@icloud.com>
2023-06-06 19:13:53 +0300

    опечатки в пути

--- bazito/users/urls.py
+++ bazito/users/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from users.views import (UserCreateView, UserDeleteView, UserUpdateView,
-                         UsersView)
+                         UsersView, LocViewSet)
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('loc', LocViewSet)
 
 urlpatterns = [
     path('', UsersView.as_view()),
     # path('<int:pk>/', UserDetailView.as_view()),
     path('create/', UserCreateView.as_view()),
-    path('<int:pk>/update', UserUpdateView.as_view()),
-    path('<int:pk>/delete', UserDeleteView.as_view()),
+    path('<int:pk>/update/', UserUpdateView.as_view()),
+    path('<int:pk>/delete/', UserDeleteView.as_view()),
     # path('cat/<int:pk>/', CatDetailView.as_view())
 ]
+
+urlpatterns += router.urls
