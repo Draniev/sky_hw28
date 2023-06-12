8f66534993bf8a767023c00d07c05ce3b858ddf2
Sergei Popov <sergei.v.popov@icloud.com>
2023-06-13 00:59:17 +0300

    JWT auth

--- bazito/users/urls.py
+++ bazito/users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from users.views import (UserCreateView, UserDeleteView, UserUpdateView,
                          UsersView, LocViewSet)
 from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 router = routers.SimpleRouter()
 router.register('loc', LocViewSet)
@@ -9,6 +11,8 @@ router.register('loc', LocViewSet)
 urlpatterns = [
     path('', UsersView.as_view()),
     # path('<int:pk>/', UserDetailView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('create/', UserCreateView.as_view()),
     path('<int:pk>/update/', UserUpdateView.as_view()),
     path('<int:pk>/delete/', UserDeleteView.as_view()),
